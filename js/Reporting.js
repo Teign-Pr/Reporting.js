58865f587f3bd62dd26476f8a2c255fdc638cb47
Teign-Pr <josh.dyer@teignbridge.co.uk>
2022-10-03 19:06:19 +0100

    [Addition] Reworked the config and added the boarderCustomisation

--- js/Reporting.js
+++ js/Reporting.js
@@ -1,29 +1,66 @@
-var CreateReport = function (config) {
-    if (config['reportName'] == null) {
+var styles = ['block', 'roof', 'side'];
+var CreateReport = function (inConfig) {
+    var config = initConfig(inConfig);
+    var reportFrame = document.getElementById(config.reportFormat.id);
+    createHtml(config, reportFrame);
+};
+var createHtml = function (config, frame) {
+    var style = config.reportFormat.borderStyle;
+    var finalStyle;
+    if (style === styles[0]) {
+        finalStyle =
+            'border-image: linear-gradient(45deg, ' +
+                config.reportFormat.borderColor1 +
+                ', '
+                + config.reportFormat.borderColor2 +
+                ') 1;';
+    }
+    else if (style === styles[1]) {
+        finalStyle =
+            'border-image: linear-gradient(to left, ' +
+                config.reportFormat.borderColor1 +
+                ', '
+                + config.reportFormat.borderColor2 +
+                ') 1 0;';
+    }
+    else if (style === styles[2]) {
+        finalStyle =
+            'border-image: linear-gradient(to bottom, ' +
+                config.reportFormat.borderColor1 +
+                ', '
+                + config.reportFormat.borderColor2 +
+                ') 0 1;';
+    }
+    else {
+        finalStyle = style;
+    }
+    frame.innerHTML = '<div class="border" style="' + finalStyle + '"> </div>';
+};
+var initConfig = function (option) {
+    if (option.reportName == null)
         throw ('Config Error: reportName is missing');
-    }
-    if (config['reportFormat'] == null) {
-        throw ('Config Error: reportFormat is missing');
-    }
-    if (config['url'] == null) {
+    if (option.url == null)
         throw ('Config Error: url is missing');
-    }
-    if (config['reportFormat']['type'] == null) {
-        config['reportFormat']['type'] = "json";
-    }
-    if (config['reportFormat']['type'] == null) {
-        config['reportFormat']['id'] = "report";
-    }
-    console.log(config["reportName"]);
-    console.log(config['reportFormat']['type']);
+    return {
+        reportName: option.reportName,
+        reportFormat: {
+            inputType: option.reportFormat.inputType || 'json',
+            id: option.reportFormat.id || 'report',
+            borderStyle: option.reportFormat.borderStyle || 'block',
+            borderColor1: option.reportFormat.borderColor1 || 'turquoise',
+            borderColor2: option.reportFormat.borderColor2 || 'greenyellow'
+        },
+        url: option.url
+    };
 };
 var test = {
     reportName: 'Test-Report',
     reportFormat: {
-        type: 'json',
-        id: 'report', // id: Html id
+        inputType: 'json',
+        id: 'report',
+        borderStyle: 'block'
     },
-    url: 'iris' // Url to get the data from
+    url: 'iris'
 };
 CreateReport(test);
 //# sourceMappingURL=Reporting.js.map
--- ts/Reporting.ts
+++ ts/Reporting.ts
@@ -1,41 +1,89 @@
-const CreateReport = (config: object) => {
-
-    if(config['reportName'] == null){
-        throw ('Config Error: reportName is missing')
-    }
-    if(config['reportFormat'] == null){
-        throw ('Config Error: reportFormat is missing')
-    }
-    if(config['url'] == null){
-        throw ('Config Error: url is missing')
-    }
-
-    if(config['reportFormat']['type'] == null){
-        config['reportFormat']['type'] = "json"
-    }
-    if(config['reportFormat']['id'] == null){
-        config['reportFormat']['id'] = "report"
-    }
-
-
-    const reportFrame = document.getElementById(config['reportFormat']['id'])
-
-    createHtml(config, reportFrame)
-
-}
-
-
-function createHtml(config: object, frame: HTMLElement){
-    frame.innerHTML = '<div class="border" style=""> </div>'
-}
-
-
-const test = {
-    reportName: 'Test-Report', // report name: what is the report called
+interface Config {
+    reportName: string,// report name: what is the report called
     reportFormat: {
-        type: 'json', // types: csv, json
-        id: 'report', // id: Html id
+        inputType?: string,// inputType: csv, json
+        id?: string,// id: Html id
+        borderStyle?: string,// borderStyle: {def: block, roof, sides}, {style: add custom style}
+        borderColor1?: string,
+        borderColor2?: string
     },
-    url: 'iris' // url: to get the data from
+    url: string// url: to get the data from
 }
-CreateReport(test)
+
+const styles : string[]= ['block', 'roof', 'side']
+
+const CreateReport = (inConfig: Partial<Config>): void => {
+    var config: Config = initConfig(inConfig);
+
+    const reportFrame = document.getElementById(config.reportFormat.id);
+
+    createHtml(config, reportFrame);
+
+}
+
+
+const createHtml = (config: Config, frame: HTMLElement): void => {
+
+    const style = config.reportFormat.borderStyle;
+    let finalStyle: string
+    if(style === styles[0]){
+        finalStyle =
+            'border-image: linear-gradient(45deg, '+
+            config.reportFormat.borderColor1 +
+            ', '
+            +config.reportFormat.borderColor2+
+            ') 1;';
+    }else if(style === styles[1]){
+        finalStyle =
+            'border-image: linear-gradient(to left, '+
+            config.reportFormat.borderColor1 +
+            ', '
+            +config.reportFormat.borderColor2+
+            ') 1 0;';
+    }else if(style === styles[2]){
+        finalStyle =
+            'border-image: linear-gradient(to bottom, '+
+            config.reportFormat.borderColor1 +
+            ', '
+            +config.reportFormat.borderColor2+
+            ') 0 1;';
+    }else{
+        finalStyle = style;
+    }
+
+    frame.innerHTML = '<div class="border" style="'+finalStyle+'"> </div>'
+
+
+}
+
+
+
+const initConfig = (option?: Partial<Config>): Config => {
+
+    if(option.reportName == null) throw ('Config Error: reportName is missing');
+    if (option.url == null) throw ('Config Error: url is missing');
+
+    return {
+        reportName: option.reportName,
+        reportFormat: {
+            inputType: option.reportFormat.inputType || 'json',
+            id: option.reportFormat.id || 'report',
+            borderStyle: option.reportFormat.borderStyle || 'block',
+            borderColor1: option.reportFormat.borderColor1 || 'turquoise',
+            borderColor2: option.reportFormat.borderColor2 || 'greenyellow'
+        },
+        url: option.url
+    };
+}
+
+
+const test: Config = {
+        reportName: 'Test-Report',
+        reportFormat: {
+            inputType: 'json',
+            id: 'report',
+            borderStyle: 'block'
+        },
+        url: 'iris'
+};
+CreateReport(test);
